0664385a379096a4d3605a38eef620992d3c7907
Peter Zhu <zhujiaxi@amazon.com>
2025-01-16 15:57:27 -0500

    Delete staging directory

--- staging/assets/js/home-listener.js
+++
@@ -1,5 +0,0 @@
-const contributeButton = document.getElementById('contribute');
-
-contributeButton.addEventListener('click', function(event) {
-    window.open('https://github.com/opensearch-project', '_blank');
-});
